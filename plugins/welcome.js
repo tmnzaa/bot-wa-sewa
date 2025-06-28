41b00430b625e896e4be25f5c6802f12a6db977c
tmnzaa <polosanpes818@gmail.com>
2025-06-28 17:57:26 +0700

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -1,7 +1,21 @@
 module.exports = async ({ sock, m, isGroup }) => {
   if (!isGroup) return;
-  if (m.messageStubType === 27) {
-    const user = m.messageStubParameters[0];
-    await sock.sendMessage(m.key.remoteJid, { text: `Selamat datang @${user.split('@')[0]} 👋`, mentions: [user] });
+
+  const msg = m.message?.conversation || '';
+  const jid = m.key.remoteJid;
+
+  const dbPath = './welcome.json';
+  if (!require('fs').existsSync(dbPath)) require('fs').writeFileSync(dbPath, '[]');
+  const db = JSON.parse(require('fs').readFileSync(dbPath));
+  let data = db.find(x => x.id === jid);
+  if (!data) {
+    data = { id: jid, on: false };
+    db.push(data);
+  }
+
+  if (msg.startsWith('.welcome')) {
+    data.on = msg.includes('on');
+    require('fs').writeFileSync(dbPath, JSON.stringify(db, null, 2));
+    return sock.sendMessage(jid, { text: `Welcome ${data.on ? 'ON' : 'OFF'}` }, { quoted: m });
   }
 };
